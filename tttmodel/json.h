5748b58c5c8c0c196a7a0128351f59fbcb0f3593
Rodney Woollett <rodney.woollett@gmail.com>
2025-07-11 16:27:57 +1200

    json.h for ttt model

--- tttmodel/json.h
+++ tttmodel/json.h
@@ -9,107 +9,46 @@ using json = nlohmann::json;
 namespace Model
 {
 
-  inline void to_json(json &jsonOut, ClientCS const &value)
+  inline void to_json(json &jsonOut, Game const &value)
   {
-    jsonOut["name"] = value.name;
-    jsonOut["host"] = value.host;
-    jsonOut["ip"] = value.ip;
-    jsonOut["connected"] = value.connected;
-    jsonOut["processId"] = value.processId;
-    jsonOut["connectedAt"] = formatDate(value.tpConnectedAt);
-    jsonOut["disconnectedAt"] = formatDate(value.tpDisconnectedAt);
-    jsonOut["parentId"] = value.parentId;
-    jsonOut["clientIp"] = value.clientIp;
-    json parent;
-    parent["id"] = value.parentId;
-    parent["clientIp"] = value.clientIp;
-    jsonOut["requestParent"] = parent;
+    jsonOut["id"] = value.id;
+    jsonOut["userId"] = value.userId;
+    jsonOut["board"] = value.board;
+    jsonOut["createdAt"] = formatDate(value.tpCreatedAt);
   }
 
-  inline void from_json(json const &jsonIn, ClientCS &value)
+  inline void from_json(json const &jsonIn, Game &value)
   {
-    jsonIn.at("name").get_to(value.name);                     //  clientCS.name = getString("name");
-    jsonIn.at("host").get_to(value.host);                     // = getString("host");
-    jsonIn.at("ip").get_to(value.ip);                         // = getString("ip");
-    jsonIn.at("connected").get_to(value.connected);           // = getString("connected").get<bool>();
-    jsonIn.at("processId").get_to(value.processId);           //= getString("processId");
-    jsonIn.at("connectedAt").get_to(value.connectedAt);       //= getString("connectedAt");
-    jsonIn.at("disconnectedAt").get_to(value.disconnectedAt); //= getString("disconnectedAt");
-    auto tpOptCA = parseDate(value.connectedAt);
-    auto tpOptDA = parseDate(value.disconnectedAt);
-    if (tpOptCA)
-      value.tpConnectedAt = *tpOptCA;
-    if (tpOptDA)
-      value.tpDisconnectedAt = *tpOptDA;
-  };
-
-  inline void to_json(json &jsonOut, ClientConnectCS const &value)
-  {
-    jsonOut["sourceIp"] = value.sourceIp;
-    jsonOut["connected"] = value.connected;
-    jsonOut["processId"] = value.processId;
-    jsonOut["connectedAt"] = formatDate(value.tpConnectedAt);
-  }
-
-  inline void from_json(json const &jsonIn, ClientConnectCS &value)
-  {
-    jsonIn.at("sourceIp").get_to(value.sourceIp);
-    jsonIn.at("processId").get_to(value.processId);
-    // If connected at date is present from the response of api request
-    if (jsonIn.contains("connectedAt"))
+    jsonIn.at("id").get_to(value.id);
+    jsonIn.at("userId").get_to(value.userId);
+    jsonIn.at("board").get_to(value.board);
+    if (jsonIn.contains("createdAt"))
     {
-      jsonIn.at("connectedAt").get_to(value.connectedAt);
-      auto tpOptCA = parseDate(value.connectedAt);
+      jsonIn.at("createdAt").get_to(value.createdAt);
+      auto tpOptCA = parseDate(value.createdAt);
       if (tpOptCA)
         value.tpConnectedAt = *tpOptCA;
     }
+  };
+
+  inline void to_json(json &jsonOut, PlayerMove const &value)
+  {
+    jsonOut["id"] = value.id;
+    jsonOut["gameId"] = value.gameId;
+    jsonOut["player"] = value.player;
+    jsonOut["moveCell"] = value.moveCell;
+    jsonOut["isOpponentStart"] = value.isOpponentStart;
+    jsonOut["allocated"] = value.allocated;
   }
 
-  inline void to_json(json &jsonOut, ClientDisconnectCS const &value)
+  inline void from_json(json const &jsonIn, PlayerMove &value)
   {
-    jsonOut["sourceIp"] = value.sourceIp;
-    jsonOut["connected"] = value.connected;
-    jsonOut["disconnectedAt"] = formatDate(value.tpDisconnectedAt);
-  }
-
-  inline void from_json(json const &jsonIn, ClientDisconnectCS &value)
-  {
-    jsonIn.at("sourceIp").get_to(value.sourceIp);
-    if (jsonIn.contains("disconnectedAt"))
-    {
-      jsonIn.at("disconnectedAt").get_to(value.disconnectedAt);
-      auto tpOpt = parseDate(value.disconnectedAt);
-      if (tpOpt)
-        value.tpDisconnectedAt = *tpOpt;
-    }
-  }
-
-  inline void to_json(json &jsonOut, CSTokenRequest const &value)
-  {
-    jsonOut["sourceIp"] = value.sourceIp;
-    jsonOut["originalIp"] = value.originalIp;
-    jsonOut["parentIp"] = value.parentIp;
-    jsonOut["relayed"] = value.relayed;
-  }
-
-  inline void from_json(json const &jsonIn, CSTokenRequest &value)
-  {
-    jsonIn.at("sourceIp").get_to(value.sourceIp);
-    jsonIn.at("originalIp").get_to(value.originalIp);
-    jsonIn.at("parentIp").get_to(value.parentIp);
-    jsonIn.at("relayed").get_to(value.relayed);
-  }
-
-  inline void to_json(json &jsonOut, CSTokenAcquire const &value)
-  {
-    jsonOut["sourceIp"] = value.sourceIp;
-    jsonOut["ip"] = value.ip;
-  }
-
-  inline void from_json(json const &jsonIn, CSTokenAcquire &value)
-  {
-    jsonIn.at("sourceIp").get_to(value.sourceIp);
-    jsonIn.at("ip").get_to(value.ip);
+    jsonIn.at("id").get_to(value.id);
+    jsonIn.at("gameId").get_to(value.gameId);
+    jsonIn.at("player").get_to(value.player);
+    jsonIn.at("moveCell").get_to(value.moveCell);
+    jsonIn.at("isOpponenentStart").get_to(value.isOpponentStart);
+    jsonIn.at("allocated").get_to(value.allocated);
   }
 
 } // namespace Model
